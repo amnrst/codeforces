90bae6460d2e3d26a74bb6c1e801cc4416baaeac
amin <amin.roosta@outlook.com>
2022-12-18 18:27:39 -0500

    1767_b

---
+++ src/bin/1767_b.rs
@@ -0,0 +1,90 @@
+// https://codeforces.com/contest/1767/problem/b
+macro_rules! input{
+    ($iter:expr) => {};
+    ($iter:expr,) => {};
+    (src = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input!{iter, $($r)*}
+    };
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input!{$iter $($r)*}
+    };
+}
+macro_rules! read_value {
+    ($iter:expr, ($($t:tt),* )) => { ( $(read_value!($iter, $t)),* ) };
+    ($iter:expr, [$t:tt;]) => { read_value!($iter, [$t; read_value!($iter, usize)]) };
+    ($iter:expr, [$t:tt;$len:expr]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::io::BufWriter;
+use std::io::Write;
+use std::str;
+
+fn run(out: &mut BufWriter<impl Write>, src: &str) {
+    input! {
+        src = src,
+        t: usize,
+        tcs:[[u32;];t]
+    }
+    let mut tcs = tcs;
+    for v in tcs.iter_mut() {
+        let mut cur = v[0];
+        v.sort();
+        for e in v {
+            if *e > cur {
+                cur = (cur + *e + 1) / 2;
+            }
+        }
+        writeln!(out, "{}", cur).unwrap();
+    }
+}
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = std::io::BufWriter::new(out.lock());
+    let s = {
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    };
+    run(&mut out, &s);
+}
+
+#[test]
+fn test_0() {
+    let vec = Vec::new();
+    let mut out = BufWriter::with_capacity(100, vec);
+    run(
+        &mut out,
+        "4
+3
+1 2 3
+3
+1 2 2
+2
+1 1000000000
+10
+3 8 6 7 4 1 2 4 10 1",
+    );
+
+    let vec = out.into_inner().unwrap();
+    let result = str::from_utf8(&vec).unwrap();
+    assert_eq!(
+        result.trim(),
+        "3
+2
+500000001
+9"
+    );
+}
+
